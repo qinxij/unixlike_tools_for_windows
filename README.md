4984a448c983306ae8ba69ce399d8702fb57cf46
qinxij <qinxij@163.com>
2013-08-17 17:20:00 +0800

    Iinitialization!

---
+++ README.md
@@ -0,0 +1,16 @@
+## Add "C:\tools\" to environment variable PATH
+wget:     http://users.ugent.be/~bpuype/wget/
+curl:	  http://curl.haxx.se/gknw.net/7.32.0/dist-w32/curl-7.32.0-rtmp-ssh2-ssl-sspi-zlib-idn-static-bin-w32.zip
+gvim:     http://lilydjwg.vim-cn.com/share/gvim74-x64.3.exe
+ln:       http://schinagl.priv.at/nt/ln/ln.html#contact
+git:      http://code.google.com/p/msysgit/downloads/list?q=full+installer+official+git
+
+
+Usage:
+  1. Download git from the url and install it.
+
+  2. Then use command: 
+
+       git clone https://github.com/qinxij/unixlike_tools_for_windows.git c:/tools
+
+  3. Add "C:\tools\" to environment varible `PATH`. Or use a .rc file powershell.
---
+++ bootstrap_windows.ps1
@@ -0,0 +1,25 @@
+echo "Download ivim ..."
+git clone https://github.com/kepbod/ivim.git $HOME/ivimgvim
+
+
+echo "Installing Vundle..."
+git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+echo "Installing hybrid theme..."
+git clone git://github.com/w0ng/vim-hybrid.git $HOME/.vim/bundle/vim-hybrid
+
+echo "Installing font for powerline..."
+mkdir $HOME/.fonts -Force
+wget -O $HOME/.fonts/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf --no-check-certificate
+mkdir $HOME/.fonts.conf.d -Force
+wget -O $HOME/.fonts.conf.d/10-powerline-symbols.conf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf --no-check-certificate 
+
+echo "Installing plugins using Vundle..."
+
+echo "ivim has been installed. Just enjoy vimming!"
+
+## link _vim and _vimrc in windows.
+ln --unroll --recursive $Home/.vim $HOME/_vim
+ln --symbolic $HOME/ivim/vimrc $HOME/_vimrc
+
+echo "Please run gvim and then run command :BundleUpdate in it."
