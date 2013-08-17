cbc383459c7409da19a381e0c26b94ee41fc18df
qinxij <qinxij@163.com>
2013-08-17 19:32:34 +0800

    Color the notification text.

--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -1,14 +1,14 @@
-echo "Download ivim ..."
+Write-Host -ForegroundColor green "Download ivim ..."
 git clone https://github.com/kepbod/ivim.git $HOME/ivimgvim
 
 
-echo "Installing Vundle..."
+Write-Host -ForegroundColor green "Installing Vundle..."
 git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
-echo "Installing hybrid theme..."
+Write-Host -ForegroundColor green "Installing hybrid theme..."
 git clone git://github.com/w0ng/vim-hybrid.git $HOME/.vim/bundle/vim-hybrid
 
-echo "Installing font for powerline..."
+Write-Host -ForegroundColor green "Installing font for powerline..."
 mkdir $HOME/.fonts -Force
 wget -O $HOME/.fonts/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf --no-check-certificate
 mkdir $HOME/.fonts.conf.d -Force
@@ -19,7 +19,7 @@ wget -O $HOME/.fonts.conf.d/10-powerline-symbols.conf https://github.com/Lokalto
 ln --unroll --recursive $Home/.vim $HOME/_vim
 ln --symbolic $HOME/ivim/vimrc $HOME/_vimrc
 
-echo "Installing plugins using Vundle..."
+Write-Host -ForegroundColor green "Installing plugins using Vundle..."
 gvim +'set nospell' +BundleInstall! +BundleClean! +qa! $HOME/ivim/tools/info.txt
 
-echo "ivim has been installed. Just enjoy vimming!"
+Write-Host -ForegroundColor green "ivim has been installed. Just enjoy vimming!"
