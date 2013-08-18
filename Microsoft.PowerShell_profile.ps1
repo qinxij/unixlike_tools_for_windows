5d77687a10e14d945cf7780006c8717c207cdcb5
qinxij <qinxij@163.com>
2013-08-18 23:13:55 +0800

    Add Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+# this file was located in `$HOME/Documents/WindowsPowerShell/`.
+$env:path += ";$home\tools;c:\tools"
+set-alias ~ $home
+set-alias gvim 'C:\Program Files (x86)\Vim\vim74\gvim'
+set-alias curl C:\Users\qij\tools\curl-openssl-libssh2\bin\curl
+import-module posh-git
