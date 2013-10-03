965fcb264f90e6c78e533bc33b6db1ef7ae1f172
qinxij <qinxij@163.com>
2013-10-04 01:36:17 +0800

    add some new and delete console2

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,20 @@
 # this file was located in `$HOME/Documents/WindowsPowerShell/`.
-. (Resolve-Path ~/Documents/WindowsPowerShell/profile.ps1)
+. (Resolve-Path $(split-Path $PROFILE) "profile.ps1")
 
-$env:path += ";$home\tools;c:\tools"
+$env:path += ";c:\tools"
 set-alias ~ $home
-set-alias gvim 'C:\Program Files (x86)\Vim\vim74\gvim'
-set-alias curl C:\Users\qij\tools\curl-openssl-libssh2\bin\curl
-import-module posh-git
+set-alias gvim 'c:\Program files\Vim\vim74\gvim'
+
+
+ls $(Join-Path $(Split-Path $PROFILE) "Modules") | %{ ipmo $_.BaseName -Force }
+
+
+if (Test-Path $HOME/workspace) {
+    mkdir $HOME/workspace -Force > $NULL
+}
 
 New-PSDrive -Name ws -PSProvider FileSystem -Root $Home/workspace > $null
-New-PSDrive -Name ps -PSProvider FileSystem -Root $Home/workspace/powershell > $null
-New-PSDrive -Name in -PSProvider FileSystem -Root $Home/workspace/powershell/install_remove_software > $null
-New-PSDrive -Name dcm -PSProvider FileSystem -Root $Home/Documents > $null
 New-PSDrive -Name hm -PSProvider FileSystem -Root $Home > $null
+New-PSDrive -Name ps -PSProvider FileSystem -Root $(Split-Path $PROFILE) > $null
+New-PSDrive -Name md -PSProvider FileSystem -Root $(Join-Path -Path $(Split-Path $PROFILE) -ChildPath "Modules") > $null
+
--- bootstrap_windows.ps1
+++ bootstrap_windows.ps1
@@ -1,5 +1,11 @@
+$tools_path = $(Split-Path $MyInvocation.MyCommand.Path)
+$ps_path = $(Split-Path $PROFILE)
+
+# reg add ConEmu configration
+reg import $(Join-Path $tools_path "ConEmu_configuration.reg")
+
 Write-Host -ForegroundColor green "Download ivim ..."
-git clone https://github.com/kepbod/ivim.git $HOME/ivimgvim
+git clone https://github.com/kepbod/ivim.git $HOME/ivim
 
 
 Write-Host -ForegroundColor green "Installing Vundle..."
@@ -15,9 +21,33 @@ mkdir $HOME/.fonts.conf.d -Force
 wget -O $HOME/.fonts.conf.d/10-powerline-symbols.conf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf --no-check-certificate 
 
 
+$more_vimrc_lines = @(cat $(Join-Path -Path $tools_path -ChildPath "more_vimrc.vim"))
+$more_vimrc_lines |
+    % { $_ -replace '##HOME##', $HOME -replace '##TOOLS##', $tools_path } |
+    Add-Content "$HOME/_vimrc"
+
+# get the contents and replace line breaks by U+000A
+$contents = [IO.File]::ReadAllText("$HOME/_vimrc") -replace "`r`n?", "`n"
+# create UTF-8 encoding without signature
+$utf8 = New-Object System.Text.UTF8Encoding $false
+# write the text back
+[IO.File]::WriteAllText("$HOME/_vimrc", $contents, $utf8)
+
+
+#     Add-Content $(Join-Path -Path $tools_path -ChildPath "more_vimrc.vim.tmp")
+# cat $(Join-Path -Path $tools_path -ChildPath "more_vimrc.vim.tmp") | Add-Content $HOME/ivim/vimrc
+# rm $(Join-Path -Path $tools_path -ChildPath "more_vimrc.vim.tmp")
+
 ## link _vim and _vimrc in windows.
-ln --unroll --recursive $Home/.vim $HOME/_vim
+ln --symbolic $Home/.vim $HOME/_vim
 ln --symbolic $HOME/ivim/vimrc $HOME/_vimrc
+
+
+mkdir $HOME/.vim/bundle/VimIM/plugin/ -Force
+if (!(Test-Path "$HOME/.vim/bundle/VimIM/plugin/vimim.wubi.txt")) {
+    wget -O $HOME/.vim/bundle/VimIM/plugin/vimim.wubi.txt http://vimim-data.googlecode.com/svn/trunk/data/vimim.wubi.txt --no-check-certificate
+}
+
 
 Write-Host -ForegroundColor green "Installing plugins using Vundle..."
 gvim +'set nospell' +BundleInstall! +BundleClean! +qa! $HOME/ivim/tools/info.txt
@@ -25,6 +55,9 @@ gvim +'set nospell' +BundleInstall! +BundleClean! +qa! $HOME/ivim/tools/info.txt
 Write-Host -ForegroundColor green "ivim has been installed. Just enjoy vimming!"
 
 ## configration for powerhsll:
-Copy-Item c:/tools/profile.ps1 $HOME/Documents/WindowsPowerShell/profile.ps1
-Copy-Item c:/tools/gitutils.ps1 $HOME/Documents/WindowsPowerShell/gitutils.ps1
-Copy-Item c:/tools/Microsoft.PowerShell_profile.ps1 $HOME/Documents/WindowsPoMicrosoft.PowerShell_profile.ps1
+Copy-Item $(Join-path -Path $tools_path -ChildPath "profile.ps1") `
+    $(Join-Path -Path $ps_path -ChildPath "profile.ps1")
+Copy-Item $(Join-path -Path $tools_path -ChildPath "gitutils.ps1") `
+    $(Join-Path -Path $ps_path -ChildPath "gitutils.ps1")
+Copy-Item $(Join-path -Path $tools_path -ChildPath "Microsoft.PowerShell_profile.ps1") `
+    $PROFILE
